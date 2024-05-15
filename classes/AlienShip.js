9407300830fe85e2cf5057c6ecad7fa0d01a40a9
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-05-15 11:48:09 -0400

    Created the player and alien classes

--- classes/AlienShip.js
+++ classes/AlienShip.js
@@ -1,16 +1,22 @@
 class AlienShip {
-    // An alien should have at least these properties:
-    // - health
-    // - power
-    // - armor
+  // An alien should have at least these properties:
+  // - health
+  // - power
+  // - armor
+  constructor() {
+    this.hp = 100;
+    this.power = Math.floor(Math.random() * 31) + 30;
+    this.armor = Math.floor(Math.random() * 20);
+  }
 
-    // And these methods
-    // - attack
-    // - defend
-    // - power up
+  // And these methods
+  // - attack
+  // - defend
+  // - power up
+
 }
 
+const alien1 = new AlienShip()
+console.log(alien1)
 
-
-
-module.exports = AlienShip
+module.exports = AlienShip;
--- classes/Player.js
+++ classes/Player.js
@@ -1,17 +1,27 @@
 class Player {
-    // A player should have at least these properties:
-    // - name
-    // - health
-    // - power
-    // - armor
-
-    // And these methods
-    // - attack
-    // - defend
-    // - power up
+  // A player should have at least these properties:
+  // - name
+  // - health
+  // - power
+  // - armor
+  constructor(name) {
+    this.name = name;
+    this.hp = 1000;
+    this.power = Math.floor(Math.random() * 51) + 50;
+    this.armor = Math.floor(Math.random() * 21) + 20;
+  }
+  // And these methods
+  // - attack
+  // - defend
+  // - power up
+  attack(target) {
+    
+  }
+  defend() {}
+  powerUp() {}
 }
 
+const player1 = new Player("bob");
+console.log(player1);
 
-
-
-module.exports = Player
+module.exports = Player;
